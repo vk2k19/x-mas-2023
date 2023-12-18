4846ab92836afd5a0b6d624a0f3205dd1f1a36f9
Vijayendra Kumar <55515410+vk2k19@users.noreply.github.com>
2023-12-18 17:15:48 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
+    basePath: isProd ? '/x-mas-2023' : '',
+    output: 'export',
+    distDir: 'dist',
+    images: {
+        unoptimized: true
+    }
 }
 
 module.exports = nextConfig

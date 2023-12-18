c3c18b40eb5119b9c5c5dd8836c1ad92c16870ab
Vijayendra Kumar <55515410+vk2k19@users.noreply.github.com>
2023-12-19 01:50:50 +0530

    Update Game.tsx

--- app/fun-zone/Quiz/Game.tsx
+++ app/fun-zone/Quiz/Game.tsx
@@ -1,3 +1,8 @@
+"use client";
+import { faCircle, faCircleDot } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useEffect, useState } from "react";
+
 export type GameProps = {
   question: {
     id: number;
@@ -12,9 +17,15 @@ export type GameProps = {
 };
 
 export const Game = ({ question, answerCallback }: GameProps) => {
+  const [selected, setSelected] = useState<number | null>(null);
+
+  useEffect(() => {
+    setSelected(null);
+  }, [question]);
+
   return (
     !!question && (
-      <form>
+      <>
         <h3 className="text-lg">
           <b>Question: </b>
           {question.label}
@@ -23,21 +34,24 @@ export const Game = ({ question, answerCallback }: GameProps) => {
           {question.answers.map((answer) => {
             return (
               <li data-testid={answer.id} key={answer.id}>
-                <label className="text-md">
-                  <input
-                    type="radio"
-                    name={question.label}
-                    value={answer.score}
-                    className="m-2"
-                    onClick={() => answerCallback(answer.score)}
+                <button
+                  className="text-md font-bold"
+                  onClick={() => {
+                    setSelected(answer.id);
+                    answerCallback(answer.score);
+                  }}
+                >
+                  <FontAwesomeIcon
+                    icon={selected === answer.id ? faCircle : faCircleDot}
+                    className="mr-1"
                   />
                   {answer.label}
-                </label>
+                </button>
               </li>
             );
           })}
         </ul>
-      </form>
+      </>
     )
   );
 };

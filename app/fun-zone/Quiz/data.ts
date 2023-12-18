b3d29d266baf2277123946680f1ed9d978c8e70d
Vijayendra Kumar <55515410+vk2k19@users.noreply.github.com>
2023-12-19 01:51:10 +0530

    Update data.ts

--- app/fun-zone/Quiz/data.ts
+++ app/fun-zone/Quiz/data.ts
@@ -1,28 +1,78 @@
-import { randomUUID } from "crypto";
-
 export const questions = [{
-    id: 1,
-    label: 'How many times did you add or left code smell?',
+    id: 0,
+    label: 'Know or have heard about christmas celebrations this year?',
     answers: [{
+        id: 0,
+        label: 'Yes',
+        score: 10
+    }, {
+        id: 1,
+        label: 'NO',
+        score: -5
+    }]
+}, {
+    id: 1,
+    label: 'How often did you the "snooze" button on your alarm clock this year?',
+    answers: [{
+        id: 0,
+        label: 'A few days',
+        score: 5
+    }, {
+        id: 1,
+        label: 'Some Days',
+        score: 0
+    }, {
         id: 2,
+        label: 'Most of the days',
+        score: -5
+    }, {
+        id: 3,
+        label: 'Not just mine, other\'s too 😛',
+        score: -10
+    }]
+}, {
+    id: 2,
+    label: 'How often do you use "Anything is fine" as your life mantra?',
+    answers: [{
+        id: 0,
         label: 'One',
         score: 5
     }, {
-        id: 3,
+        id: 1,
         label: 'Too Many',
         score: 0
     }, {
-        id: 4,
-        label: 'Don\'t Know',
+        id: 2,
+        label: 'Don\'t Know 😰',
         score: -10
     }, {
-        id: 5,
+        id: 3,
         label: 'Never',
         score: 10
     }]
 }, {
-    id: 6,
-    label: 'How many times did you go above and beyond?',
+    id: 3,
+    label: 'How many times have you pretended to be on a conference call to avoid someone or something?',
+    answers: [{
+        id: 0,
+        label: 'A few',
+        score: 5
+    }, {
+        id: 1,
+        label: 'Too Many',
+        score: -10
+    }, {
+        id: 2,
+        label: 'Don\'t Know',
+        score: 0
+    }, {
+        id: 3,
+        label: 'Never',
+        score: 10
+    }]
+}, {
+    id: 4,
+    label: 'How often do you dance like nobody\'s watching when your favorite song comes on?',
     answers: [{
         id: 7,
         label: 'A few',
@@ -37,87 +87,247 @@ export const questions = [{
         score: 5
     }, {
         id: 10,
-        label: 'Never',
+        label: 'Not at all',
         score: 0
+    }]
+}, {
+    id: 5,
+    label: 'How many times have you sent a funny meme to the wrong person?',
+    answers: [{
+        id: 0,
+        label: 'A few',
+        score: 5
+    }, {
+        id: 1,
+        label: 'Too Many',
+        score: 0
+    }, {
+        id: 2,
+        label: 'Don\'t Know',
+        score: -5
+    }, {
+        id: 3,
+        label: 'Never',
+        score: 10
+    }]
+}, {
+    id: 6,
+    label: 'Did you learn something new this year?',
+    answers: [{
+        id: 0,
+        label: 'A few',
+        score: 5
+    }, {
+        id: 1,
+        label: 'Too Many',
+        score: 10
+    }, {
+        id: 2,
+        label: 'Don\'t Know',
+        score: 0
+    }, {
+        id: 3,
+        label: 'Not at all',
+        score: -10
+    }]
+}, {
+    id: 7,
+    label: 'How many times have you laughed at a joke you didn\'t understand, just to fit in?',
+    answers: [{
+        id: 0,
+        label: 'A few days',
+        score: 5
+    }, {
+        id: 1,
+        label: 'Some Days',
+        score: 0
+    }, {
+        id: 2,
+        label: 'Most of the days',
+        score: -5
+    }, {
+        id: 3,
+        label: 'Not just others, mine\'s too 😛',
+        score: -10
+    }]
+}, {
+    id: 8,
+    label: 'Finished my goals on time',
+    answers: [{
+        id: 0,
+        label: 'A few',
+        score: 5
+    }, {
+        id: 1,
+        label: 'Too Many',
+        score: 10
+    }, {
+        id: 2,
+        label: 'Don\'t Know',
+        score: 0
+    }, {
+        id: 3,
+        label: 'Not at all',
+        score: -10
+    }]
+}, {
+    id: 9,
+    label: 'How many times have you taken a nap at work and blamed it on lunch?',
+    answers: [{
+        id: 0,
+        label: 'A few',
+        score: 5
+    }, {
+        id: 1,
+        label: 'Too Many',
+        score: -10
+    }, {
+        id: 2,
+        label: 'Don\'t Know',
+        score: 0
+    }, {
+        id: 3,
+        label: 'Not at all',
+        score: 10
+    }]
+}, {
+    id: 10,
+    label: 'Have you ever accidentally sent a voice message that was just you making weird noises?',
+    answers: [{
+        id: 0,
+        label: 'A few',
+        score: 5
+    }, {
+        id: 1,
+        label: 'Too Many',
+        score: -10
+    }, {
+        id: 2,
+        label: 'Don\'t Know',
+        score: 0
+    }, {
+        id: 3,
+        label: 'Not at all',
+        score: 10
     }]
 }, {
     id: 11,
-    label: 'How many times did you help your peers?',
+    label: 'Have you ever taken a detour to avoid traffic and ended up more lost?',
     answers: [{
-        id: 12,
+        id: 0,
         label: 'A few',
-        score: 10
+        score: 5
     }, {
-        id: 13,
+        id: 1,
         label: 'Too Many',
-        score: 20
+        score: -10
     }, {
-        id: 14,
+        id: 2,
+        label: 'Don\'t Know',
+        score: 0
+    }, {
+        id: 3,
+        label: 'Not at all',
+        score: 10
+    }]
+}, {
+    id: 12,
+    label: 'Have you ever used the excuse "network issues" to avoid a video call?',
+    answers: [{
+        id: 0,
+        label: 'A few',
+        score: 5
+    }, {
+        id: 1,
+        label: 'Too Many',
+        score: -10
+    }, {
+        id: 2,
+        label: 'Don\'t Know',
+        score: 0
+    }, {
+        id: 3,
+        label: 'Not at all',
+        score: 10
+    }]
+}, {
+    id: 13,
+    label: 'How many times have you used "Forgot my wallet" to avoid splitting the bill?',
+    answers: [{
+        id: 0,
+        label: 'A few',
+        score: 0
+    }, {
+        id: 1,
+        label: 'Too Many',
+        score: -10
+    }, {
+        id: 2,
         label: 'Don\'t Know',
         score: 5
     }, {
-        id: 15,
-        label: 'Never',
+        id: 3,
+        label: 'Not at all',
+        score: 10
+    }]
+}, {
+    id: 14,
+    label: 'How often do you use "Will Diet/GYM from tomorrow" but never actually start?',
+    answers: [{
+        id: 0,
+        label: 'A few',
         score: 0
+    }, {
+        id: 1,
+        label: 'Too Many',
+        score: -10
+    }, {
+        id: 2,
+        label: 'Don\'t Know',
+        score: -5
+    }, {
+        id: 3,
+        label: 'Not at all',
+        score: 10
+    }]
+}, {
+    id: 15,
+    label: 'How many times have you laughed at a joke in a movie theater and realized you were the only one?',
+    answers: [{
+        id: 0,
+        label: 'A few',
+        score: 5
+    }, {
+        id: 1,
+        label: 'Too Many',
+        score: 10
+    }, {
+        id: 2,
+        label: 'Don\'t remember',
+        score: 0
+    }, {
+        id: 3,
+        label: 'Never, I don\'t laugh in a theater',
+        score: -5
     }]
 }, {
     id: 16,
-    label: 'How many times you had unhealthy arguments with your FnF',
+    label: 'How often do you invent your own lyrics to songs when you don\'t know the words?',
     answers: [{
-        id: 17,
-        label: 'A few',
+        id: 0,
+        label: 'Few times',
+        score: 10
+    }, {
+        id: 1,
+        label: 'Mostly',
+        score: 5
+    }, {
+        id: 2,
+        label: 'I Never sing 😛',
         score: -5
     }, {
-        id: 18,
-        label: 'Too Many',
-        score: -10
-    }, {
-        id: 19,
-        label: 'Don\'t Know',
-        score: 0
-    }, {
-        id: 20,
-        label: 'Never',
-        score: 10
-    }]
-}, {
-    id: 21,
-    label: 'Did you learn something new this year?',
-    answers: [{
-        id: 22,
-        label: 'A few',
-        score: 5
-    }, {
-        id: 23,
-        label: 'Too Many',
-        score: 10
-    }, {
-        id: 24,
-        label: 'Don\'t Know',
-        score: 0
-    }, {
-        id: 25,
-        label: 'Not at all',
-        score: -10
-    }]
-}, {
-    id: 26,
-    label: 'Finished sprint goals on time',
-    answers: [{
-        id: 27,
-        label: 'A few',
-        score: 5
-    }, {
-        id: 28,
-        label: 'Too Many',
-        score: 10
-    }, {
-        id: 29,
-        label: 'Don\'t Know',
-        score: 0
-    }, {
-        id: 30,
-        label: 'Not at all',
+        id: 3,
+        label: 'Never, I know songs',
         score: -10
     }]
 }]

0332105d2e9100756c9bf1a7d998eac98af30dd2
Vijayendra Kumar <55515410+vk2k19@users.noreply.github.com>
2023-12-18 16:47:44 +0530

    Update page.tsx

--- page.tsx
+++ page.tsx
@@ -1,4 +1,13 @@
-import Tabs from "./fun-zone/Tabs/Tabs";
+import Tabs from "../app/fun-zone/Tabs/Tabs";
+
+export default function Home() {
+  return (
+    <main className="flex flex-col flex-1 items-start justify-start md:px-8">
+      <Tabs />
+    </main>
+  );
+}
+
 
 export default function Home() {
   return (
